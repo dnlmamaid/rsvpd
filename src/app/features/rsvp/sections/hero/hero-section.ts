e565fbd5181b0441425ee750d8a1685615a285a5
danma <dnlmamaid@gmail.com>
2026-04-21 17:58:44 +0800

    update hero background implem

--- src/app/features/rsvp/sections/hero/hero-section.ts
+++ src/app/features/rsvp/sections/hero/hero-section.ts
@@ -1,9 +1,14 @@
 import { Component } from "@angular/core";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: "app-hero-section",
-  imports: [],
+  imports: [CommonModule],
   templateUrl: "./hero-section.html",
   styleUrl: "./hero-section.scss",
 })
-export class HeroSection {}
+export class HeroSection {
+  readonly backgroundStyle = {
+    backgroundImage: `url(assets/images/hero-bg.png)`
+  };
+}

b93122ed582eb8e1263b8e2195d15843398dd52d
danma <dnlmamaid@gmail.com>
2026-04-21 19:34:31 +0800

    add invalid code redirection

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
+import {inviteGuard} from "./core/guards/invite-guard";
 
 export const routes: Routes = [
     {
         path: '',
         loadComponent: () =>
-            import('./features/rsvp/rsvp.page').then(m => m.RsvpPage)
+            import('./features/rsvp/rsvp.page').then(m => m.RsvpPage),
+        canActivate: [inviteGuard]
     }
 ];

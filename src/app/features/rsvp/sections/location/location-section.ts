55ad852dddd2e2bd230ddb96d3f96879d37685f6
danma <dnlmamaid@gmail.com>
2026-05-26 23:56:54 +0800

    update location section

--- src/app/features/rsvp/sections/location/location-section.ts
+++ src/app/features/rsvp/sections/location/location-section.ts
@@ -1,47 +1,41 @@
-import { Component, signal } from "@angular/core";
-import { LucideAngularModule } from "lucide-angular";
-import {CommonModule} from "@angular/common";
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { LucideAngularModule } from 'lucide-angular';
 
 interface Venue {
   label: string;
   name: string;
-  description: string;
   image: string;
   address: string;
   maps: string;
 }
 
 @Component({
-  selector: "app-location-section",
-  imports: [LucideAngularModule, CommonModule],
-  templateUrl: "./location-section.html",
-  styleUrl: "./location-section.scss",
+  selector: 'app-location-section',
+  imports: [LucideAngularModule],
+  templateUrl: './location-section.html',
+  styleUrl: './location-section.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LocationSection {
-  readonly title = signal("Lokasyon");
+  readonly title = signal('Lokasyon');
+
   readonly mapIconName = 'map-pin';
   readonly mapIconSize = 20;
 
-  readonly subtitle = signal(
-    "Narito ang mga lugar para sa seremonya at salu-salo."
-  );
-
   readonly venues = signal<Venue[]>([
     {
-      label: "Seremonya",
-      name: "Our Lady of Mt. Carmel Parish",
-      description: "Dito gaganapin ang pag-iisang dibdib.",
-      image: "assets/images/church-carmel.png",
-      address: "Sabang, Baliwag, Bulacan",
-      maps: "https://maps.app.goo.gl/bAYzQcj3xDUmVLBz6"
+      label: 'Seremonya',
+      name: 'Our Lady of Mt. Carmel Parish',
+      image: 'assets/images/church-carmel.png',
+      address: 'Sabang, Baliwag, Bulacan',
+      maps: 'https://maps.app.goo.gl/bAYzQcj3xDUmVLBz6',
     },
     {
-      label: "Salusalo",
-      name: "Maravilloza Events Center",
-      description: "Dito susundan ang salusalo at pagdiriwang.",
-      image: "assets/images/reception-maravilloza.png",
-      address: "Dampol I, Pulilan, Bulacan",
-      maps: "https://maps.app.goo.gl/7mV1wLzxuCSYDnFP9"
+      label: 'Salusalo',
+      name: 'Maravilloza Events Center',
+      image: 'assets/images/reception-maravilloza.png',
+      address: 'Dampol I, Pulilan, Bulacan',
+      maps: 'https://maps.app.goo.gl/7mV1wLzxuCSYDnFP9',
     },
   ]);
 

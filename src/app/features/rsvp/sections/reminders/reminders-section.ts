cf6e70a9e6d2adf15d0a76da1dfa2dc5be916ab7
danma <dnlmamaid@gmail.com>
2026-05-27 01:47:53 +0800

    update background images

--- src/app/features/rsvp/sections/reminders/reminders-section.ts
+++ src/app/features/rsvp/sections/reminders/reminders-section.ts
@@ -1,27 +1,28 @@
-import { Component } from '@angular/core';
-import {CommonModule} from "@angular/common";
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-reminders-section',
-  imports: [CommonModule],
+  imports: [NgStyle],
   templateUrl: './reminders-section.html',
   styleUrl: './reminders-section.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RemindersSection {
   readonly title = 'Paalala';
 
   readonly coupleImage = {
-    backgroundImage: `url(assets/images/dan-pat.jpg)`,
+    backgroundImage: 'url(assets/images/dan-pat.jpg)',
     backgroundSize: 'cover',
-    backgroundPosition: 'center'
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
   };
-
-
 
   readonly leadLines = [
     'Huwag kalimutang mag-RSVP',
     'bago o sa ika-23 ng Nobyembre 2026.',
   ];
-  readonly assist = `Para sa iba pang katanungan, message niyo lang kami.`;
-  readonly closing = `Kitakits sa araw ng aming pag-iisang dibdib!`;
+
+  readonly assist = 'Para sa iba pang katanungan, message niyo lang kami.';
+  readonly closing = 'Kitakits sa araw ng aming pag-iisang dibdib!';
 }
